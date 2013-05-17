2a2e1466cea6fac69ccf587822ef96c6577a0abc
Jaap <jaap.lous@informaat.nl>
2013-05-17 13:55:02 +0200

    added some comments

--- src/statemachine.js
+++ src/statemachine.js
@@ -1,25 +1,46 @@
 define(['knockout', 'underscore'], function(ko, _) {
 
-	function array(val /* Any */) {
+	/*
+		turns a value in an array containing the value
+	 */
+	function array(val) {
 		return _.isArray(val) ? val : [val];
 	}
 
-	function initial(states /* Array[State] */) {
+	/*
+		states: an array of Pachinko.State instances
+	*/
+	function initial(states) {
 		return _.find(states, function (state) {
 			return state.initial();
 		});
 	}
 
-	return function(states /* Array[State] */, triggers /* Observable */) {
+	/*
+		states: an array of Pachinko.State instances
+		triggers: an array of knockout observables
+	 */
+	return function(states, triggers) {
 		var initialState = initial(states);
-
 		if (!initialState) throw Error('No initial state specified');
 
-		var currentState = ko.observable(initialState),
-			path = ko.observableArray([initialState.name()]);
+		/*
+			contains the currentState object
+		 */
+		var currentState = ko.observable(initialState);
 
+		/*
+			array of state names, contains the path followed including the current state
+		*/
+		var path = ko.observableArray([initialState.name()]);
+
+		/*
+			evaluates what the current state should be based on the
+			evaluation of transitions and keeps track of the path
+			followed through the state diagram
+		 */
 		function evaluateData() {
-			var state = initial(states);
+			var state = initialState;
 			path([]);
 
 			do  {
@@ -29,21 +50,34 @@ define(['knockout', 'underscore'], function(ko, _) {
 
 			currentState(state);
 		}
-
-		function isInPath(name /* String */) {
+		/*
+			name: string
+			returns true the names is in the path leading to the current state
+		 */
+		function isInPath(name) {
 			return _.indexOf(path(), name) > -1;
 		}
 
+		/*
+			if one of triggers changes value, evaluate the data
+		 */
 		_.invoke(triggers, 'subscribe', evaluateData);
 
 		return {
 			evaluate: evaluateData,
 
+			/*
+				returns the name of the current state
+			*/
 			stateName: ko.computed(function() {
 				return currentState() ? currentState().name() : '';
 			}),
 
-			stateIs: function(names /* Array[String] */) {
+			/*
+				names: an array of strings
+				returns true if one of the names is the current state name
+			 */
+			stateIs: function(names) {
 				return _.indexOf(array(names), currentState().name()) > -1;
 			},
 
